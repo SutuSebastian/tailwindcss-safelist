478422db53f63109a3266b35e048476391491be7
SutuSebastian <sebiitv@gmail.com>
2025-01-27 22:23:26 +0200

    v4 vite prefix

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ export default function App() {
   const [color, setColor] = useState<"red" | "blue">("blue");
 
   return (
-    <div className="p-4 bg-gray-100 dark:bg-gray-800">
+    <div className="tw:p-4 tw:bg-gray-100 tw:dark:bg-gray-800">
       <button
-        className="border rounded px-1"
+        className="tw:border tw:rounded tw:px-1"
         onClick={() => setColor((state) => (state === "blue" ? "red" : "blue"))}
       >
         toggle color
       </button>
       <h1>color: bg-{color}-400</h1>
-      <div className={`w-full h-40 bg-${color}-400`}>content here</div>
+      <div className={`tw:w-full tw:h-40 tw:bg-${color}-400`}>content here</div>
     </div>
   );
 }
