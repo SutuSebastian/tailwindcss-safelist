26252856efddeca7323cd692946fe9af9737d72e
SutuSebastian <sebiitv@gmail.com>
2025-01-27 22:01:00 +0200

    init with v3

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import safelistPlugin from "./safelist-plugin";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [safelistPlugin],
+};
