a60c6811157a479fb60b1071759a9ac66aa76d59
SutuSebastian <sebiitv@gmail.com>
2025-01-27 22:21:15 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ export default function App() {
   return (
     <div className="tw-p-4 tw-bg-gray-100 tw-dark:bg-gray-800">
       <button
-        className="border rounded px-1"
+        className="tw-border tw-rounded tw-px-1"
         onClick={() => setColor((state) => (state === "blue" ? "red" : "blue"))}
       >
         toggle color
       </button>
       <h1>color: tw-bg-{color}-400</h1>
-      <div className={`w-w-full tw-h-40 tw-bg-${color}-400 `}>content here</div>
+      <div className={`tw-w-full tw-h-40 tw-bg-${color}-400`}>content here</div>
     </div>
   );
 }
